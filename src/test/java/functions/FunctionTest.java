dac0f14afecee1ce9a6562a73fe1ee33cbd82b2f
Riguz Lee <dr@riguz.com>
2023-09-20 20:27:18 +0800

    updated test

--- src/test/java/functions/FunctionTest.java
+++ src/test/java/functions/FunctionTest.java
@@ -27,6 +27,13 @@ public class FunctionTest {
         ChatResponse response = objectMapper.readValue("{\"created\":1695176892,\"model\":\"abab5.5-chat\",\"reply\":\"Who am I?\",\"choices\":[{\"finish_reason\":\"stop\",\"messages\":[{\"sender_type\":\"BOT\",\"sender_name\":\"MM智能助理\",\"text\":\"Who am I?\"}]}],\"usage\":{\"total_tokens\":159},\"input_sensitive\":false,\"output_sensitive\":false,\"id\":\"01598bbbeabf180700e089b7ee15fd0a\",\"base_resp\":{\"status_code\":0,\"status_msg\":\"\"}}", ChatResponse.class);
         assertEquals(0, response.getBaseResp().getStatusCode());
         assertEquals("", response.getBaseResp().getStatusMsg());
+        assertEquals("01598bbbeabf180700e089b7ee15fd0a", response.getId());
+        assertEquals("Who am I?", response.getReply());
+        assertEquals(1, response.getChoices().size());
+        assertEquals("stop", response.getChoices().get(0).getFinishReason());
+        assertEquals("Who am I?", response.getChoices().get(0).getMessages().get(0).getText());
+        assertEquals(159, response.getUsage().getTotalTokens());
+
     }
 
     @Test
